ed2c3e2bd996bb9addff6afa6be5b0c320b4b223
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-09-29 15:09:18 -0700

    Testing http call to WordNik and disabling ATS for http call.   Edited info.plist - Bummer deal

--- HangDroidMobile/ViewController.swift
+++ HangDroidMobile/ViewController.swift
@@ -27,5 +27,18 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func getWord(sender: AnyObject) {
+        fetchWord();
+    }
+    
+    func fetchWord() {
+        let url = NSURL(string:"http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=10000&maxCorpusCount=-1&minDictionaryCount=20&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=b68d7ebcb9910f3b8700c0b331f02428eb2fc4e009e697928");
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let fetchData = NSData(contentsOfURL: url!);
+            print(fetchData);
+        }
+        
+    }
 }
 
