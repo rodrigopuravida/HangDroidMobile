0f0cd7e24c76f1e5c90e4cfebe4f885663b59314
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-10-02 16:49:33 -0700

    Adding Word model

---
+++ HangDroidMobile/Word.swift
@@ -0,0 +1,18 @@
+//
+//  Word.swift
+//  HangDroidMobile
+//
+//  Created by Rodrigo Carballo on 10/2/15.
+//  Copyright © 2015 Rodrigo Carballo. All rights reserved.
+//
+
+import Foundation
+
+struct Word {
+    let word: String
+    
+    
+    init(word: String) {
+        self.word = word
+    }
+}
